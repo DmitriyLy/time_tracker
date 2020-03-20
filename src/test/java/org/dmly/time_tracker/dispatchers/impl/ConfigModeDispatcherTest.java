b51dc23cc094992e82da95ef395f1e9e95aa80c6
DmitriyLy <dmitriy.lisay@gmail.com>
2020-03-20 22:56:43 +0200

    tests setup

---
+++ src/test/java/org/dmly/time_tracker/dispatchers/impl/ConfigModeDispatcherTest.java
@@ -0,0 +1,22 @@
+package org.dmly.time_tracker.dispatchers.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConfigModeDispatcherTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void dispatch() {
+    }
+}
