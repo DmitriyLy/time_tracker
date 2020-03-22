fd2521ce396d59ca407e79074fb57b0af681df24
DmitriyLy <dmitriy.lisay@gmail.com>
2020-03-22 16:29:17 +0200

    help fixed

--- src/main/java/org/dmly/time_tracker/dispatchers/impl/HelpModeDispatcher.java
+++ src/main/java/org/dmly/time_tracker/dispatchers/impl/HelpModeDispatcher.java
@@ -5,13 +5,14 @@ import org.dmly.time_tracker.dispatchers.Dispatcher;
 public class HelpModeDispatcher implements Dispatcher {
     @Override
     public void dispatch() {
-        System.out.println("Time tracker manual:\n");
+        System.out.println("\nTime tracker manual.\n");
         System.out.println("Intervals example (without quotes):");
         System.out.println("'10m' - 10 minutes");
         System.out.println("'11s' - 11 seconds");
         System.out.println("'15m 12s' - 15 minutes and 12 seconds");
         System.out.println(" ");
-        System.out.println("Interval list example:\n");
+        System.out.println("Interval list example:");
         System.out.println("[10m, 5m, 3s, 12m 10s]");
+        System.out.println(" ");
     }
 }
