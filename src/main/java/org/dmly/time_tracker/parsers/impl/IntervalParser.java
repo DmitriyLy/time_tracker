b059069c750a5e148ffeeffaf05dbe41bd57dd7f
DmitriyLy <dmitriy.lisay@gmail.com>
2020-03-22 17:46:03 +0200

    time parsing implemented

--- src/main/java/org/dmly/time_tracker/parsers/impl/IntervalParser.java
+++ src/main/java/org/dmly/time_tracker/parsers/impl/IntervalParser.java
@@ -5,10 +5,11 @@ import org.dmly.time_tracker.parsers.Parser;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 public class IntervalParser implements Parser {
     @Override
-    public Integer parseInterval(String value) {
+    public Integer parseInterval(final String value) {
 
         if (Objects.isNull(value)) {
             throw new RuntimeException("Passed value is null.");
@@ -18,13 +19,45 @@ public class IntervalParser implements Parser {
             return 0;
         }
 
+        String[] tokens = normalizeInputValue(value).split(" ");
 
 
-        return 0;
+        return Stream.of(tokens)
+                .map(s -> {
+                    int multiplier = getMultiplierFromToken(s);
+                    int timeValue = getTimeValueFromToken(s);
+                    return multiplier * timeValue;
+                })
+                .reduce(0, (a, b) -> a + b);
     }
 
     @Override
     public List<Integer> parseIntervalList(String value) {
         return null;
+    }
+
+    private String normalizeInputValue(final String value) {
+        return StringUtils.strip(value).toLowerCase();
+    }
+
+    private int getMultiplierFromToken(final String token) {
+        if (token.endsWith("m")) return 60;
+        if (token.endsWith("s")) return 1;
+        return 0;
+    }
+
+    private int getTimeValueFromToken(final String token) {
+
+        if (token.length() < 2) {
+            return 0;
+        }
+
+        String initValue = token.substring(0, token.length() - 1);
+
+        try {
+            return Integer.parseInt(initValue);
+        } catch (NumberFormatException e) {
+            return 0;
+        }
     }
 }
--- src/test/java/org/dmly/time_tracker/parsers/impl/IntervalParserTest.java
+++ src/test/java/org/dmly/time_tracker/parsers/impl/IntervalParserTest.java
@@ -22,12 +22,51 @@ class IntervalParserTest {
     }
 
     @Test
-    void parseInterval_valueNull() {
+    void valueNull() {
         Assertions.assertThrows(RuntimeException.class, () -> parser.parseInterval(null));
     }
 
     @Test
-    void parseInterval_valueEmptyString() {
+    void valueEmptyString() {
         Assertions.assertEquals(0, parser.parseInterval(""));
+    }
+
+    @Test
+    void correctSeconds() {
+        Assertions.assertEquals(1, parser.parseInterval("1s"));
+        Assertions.assertEquals(2, parser.parseInterval("2s"));
+        Assertions.assertEquals(21, parser.parseInterval("21s"));
+        Assertions.assertEquals(213, parser.parseInterval("213s"));
+
+        Assertions.assertEquals(125, parser.parseInterval("  125s "));
+    }
+
+    @Test
+    void correctMinutes() {
+        Assertions.assertEquals(60, parser.parseInterval("1m"));
+        Assertions.assertEquals(2 * 60, parser.parseInterval("2m"));
+        Assertions.assertEquals(21 * 60, parser.parseInterval("21m"));
+        Assertions.assertEquals(213 * 60, parser.parseInterval("213m"));
+
+        Assertions.assertEquals(125 * 60, parser.parseInterval("  125m "));
+    }
+
+    @Test
+    void correctMinutesAndSeconds() {
+        Assertions.assertEquals(10 * 60 + 21, parser.parseInterval("10m 21s"));
+        Assertions.assertEquals(7 * 60 + 1, parser.parseInterval("7m 1s"));
+        Assertions.assertEquals(125 * 60 + 125, parser.parseInterval("  125m 125s   "));
+    }
+
+    @Test
+    void incorrectValues() {
+        Assertions.assertEquals(0, parser.parseInterval(" "));
+        Assertions.assertEquals(0, parser.parseInterval(" test"));
+        Assertions.assertEquals(0, parser.parseInterval(" m s"));
+        Assertions.assertEquals(0, parser.parseInterval(" ~Ghyijo.'.wf,"));
+
+        Assertions.assertEquals(7 * 60, parser.parseInterval("7m s"));
+        Assertions.assertEquals(25, parser.parseInterval(" m 25s"));
+
     }
 }
